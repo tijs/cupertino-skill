4f49a6e35f6b411c9e1c5faa104dc425b9f2f2f1
Mihaela Mihaljevic <mihaelamj@me.com>
2025-12-05 23:33:11 +0100

    upped version

--- Packages/Sources/Shared/Constants.swift
+++ Packages/Sources/Shared/Constants.swift
@@ -138,7 +138,7 @@ extension Shared {
             public static let userAgent = "CupertinoCrawler/1.0"
 
             /// Current version
-            public static let version = "0.3.0"
+            public static let version = "0.3.4"
         }
 
         // MARK: - Display Names
